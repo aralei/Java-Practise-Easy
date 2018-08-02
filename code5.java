2f3ddeeb094880abe80b11e9ecfee29cbcf46210
aralei <38710412+aralei@users.noreply.github.com>
2018-08-02 13:45:04 +0900

    Update code5.java

--- code5.java
+++ code5.java
@@ -21,3 +21,46 @@ class MyCode5 implements InterfaceC
   System.out.print(faceA);
     }
 }
+
+
+//以下为改良版
+//添加抽象方法
+//インタフェースは抽象クラスと同様に処理の中身がなくメソッド名や引数だけを定義する
+interface InterfaceA
+{
+  public String faceA = "faceA";
+  abstract void ShowA();
+}
+
+interface InterfaceB extends InterfaceA
+{
+  public String faceB = "faceB";
+  abstract void ShowB();
+}
+
+interface InterfaceC extends InterfaceB
+{
+  public String faceC = "faceC";
+  abstract void ShowC();
+}
+
+class MyCode5 implements InterfaceC
+{
+  public void ShowA(){
+    System.out.println(faceA);
+  }
+  public void ShowB(){
+    System.out.println(faceB);
+  }
+  public void ShowC(){
+    System.out.println(faceC);
+  }
+
+  public static void main(String[] args){
+  //インスタンス
+    MyCode5 my = new MyCode5();
+    my.ShowA();
+    my.ShowB();
+    my.ShowC();
+    }
+}
